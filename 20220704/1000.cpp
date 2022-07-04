8a87ab5a2551a837a30af0769e386ca153535f45
livekill93 <livekill93@gmail.com>
2022-07-04 23:53:12 +0900

    라킬
    연습연습

---
+++ 20220704/1000.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+
+main(){
+    int i,j;
+scanf("%d %d",&i,&j);
+
+printf("%d",i+j);
+    return 0;
+}
---
+++ 20220704/10172.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+main(){
+printf("|\\_/| \n|q p|   /} \n( 0 )\"\"\"\\ \n|\"^\"`    | \n||_/=\\\\__|");
+return 0;
+}
+
