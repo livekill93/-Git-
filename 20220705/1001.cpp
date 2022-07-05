4c1d841c5a920e6fd1d3e269cd684a59ccf158d9
livekill93 <livekill93@gmail.com>
2022-07-05 22:09:53 +0900

    라킬
    ;이나 ' 을 찍는것을 잊지말아야 한다

---
+++ 20220705/1001.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+main(){
+    int a,b;
+    printf("입력");
+    scanf("%d %d",&a,&b);
+    printf("%d",a-b);
+
+
+    return 0;
+}
---
+++ 20220705/1008.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+double a,b;
+    scanf("%lf %lf",&a,&b);
+    printf("%.13lf", a/b);// ,찍는것을 잊지말자
+
+
+    return 0;
+}
