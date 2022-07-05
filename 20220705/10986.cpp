e61ffd7b1fcd0904534c92c28a9be5db2774c9dc
livekill93 <livekill93@gmail.com>
2022-07-05 22:57:10 +0900

    라킬

---
+++ 20220705/10986.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    char A[51];
+
+    scanf("%s",A);
+    printf("%s??!",A);
+
+
+
+    return 0;
+}
---
+++ 20220705/18108.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std
+int main(){
+    int y;
+    cin >> y;
+    cout << a - 543;
+
+
+
+    return 0;
+}
