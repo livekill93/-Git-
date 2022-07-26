f9015c8e37305c0e0e0f80484e6f13b73725bbc5
livekill93 <livekill93@gmail.com>
2022-07-26 19:00:43 +0900

    라킬

--- 20220725/2675.cpp
+++ 20220725/2675.cpp
@@ -6,13 +6,13 @@ using namespace std;
 int main(){
     int i,j,k;
     string alphanumeric="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\\$%*+-./:";
-    string S;
+    string S[26];
     int T,R;
 
     cin >> T;
     for(i=0;i<T;i++){
         cin >> R  ;
-		cin >> S;
+		cin >> S[j];
         
         for(j=0;j<R;j++){
             for( k=0;k<R;k++){
