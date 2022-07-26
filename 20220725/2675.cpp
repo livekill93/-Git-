3d423aa969a665c668f7a32c0a135475bf8f40e9
livekill93 <livekill93@gmail.com>
2022-07-26 22:49:56 +0900

    라킬
    2675번 풀었음

--- 20220725/2675.cpp
+++ 20220725/2675.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 int main(){
     int i,j,k;
-    string alphanumeric="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\\$%*+-./:";
-    string S[26];
+  /*  string alphanumeric="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\\$%*+-./:";*///끝이 null함수이기때문에 문제가 발생함
+    string S;
     int T,R;
 
     cin >> T;
     for(i=0;i<T;i++){
-        cin >> R  ;
-		cin >> S[j];
+        cin >> R ;
+		cin >> S;
         
-        for(j=0;j<R;j++){
+        for(j=0;j<S.size();j++){//이부분을 수정해준닼
             for( k=0;k<R;k++){
                 cout << S[j];
             }
