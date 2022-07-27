99fcfe9d556d6d48c09e14ad57df69e0e1fc15e6
livekill93 <livekill93@gmail.com>
2022-07-27 14:02:43 +0900

    라킬
    상수

---
+++ 20220727/1152.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#define N 1000001
+//띄어쓰기를 카운트하면 되는거 아닐까? 거기에 끝에 +1로다가
+using namespace std;
+
+int main(){
+    string str;
+    int cnt =0 ;
+
+    getline(cin,str);
+
+    for(int i=0;i<str.length();i++){
+        if(str[i] == ' '){
+                cnt++;
+        }
+    }
+    if(str[0]== ' ')
+        cnt--;
+    if(str[str.length() -1]== ' ')
+        cnt--;
+    
+    cout << cnt;
+    return 0;
+}
---
+++ 20220727/2908.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+//두수를 뒤집은 다음에 크기르 비교해서 큰거 출력?
+using namespace std;
+
+int main(){
+    string a,b;
+
+    cin >> a >> b ;
+
+    string c;
+
+    for(int i=3; i>0;i++){
+        if(a[i]>b[i]){
+            c=a;
+            break;
+        }
+        else if(a[i]<b[i]){
+            c=b;
+            break;
+        }
+    }
+    cout << c[2] << c[1] << c[0] ;
+
+}
