9d2d8eb1518a9cc2d3388e2fb260c46fa2d36879
livekill93 <livekill93@gmail.com>
2022-07-28 22:37:57 +0900

    라킬
    입력 wa와 WA는 다르다

--- 20220728/5622.cpp
+++ 20220728/5622.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 int main(){
     string arr;
-    int num[] = {3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9,9,10,10,10};
+ 
+    int num[] = {3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10};
     int result = 0;
 
     getline(cin, arr);
 
-    for(int i= 0 ; arr[i] != '\0';i++  ){
+    for(int i= 0 ; i<arr.length();i++  ){
         result += num[arr[i] - 'A'];
     }
     cout << result <<endl;
