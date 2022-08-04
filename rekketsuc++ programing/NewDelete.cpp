80e22fed531888f274690525bcd9cf499b4f5fa6
livekill93 <livekill93@gmail.com>
2022-08-04 23:16:50 +0900

    라킬
    nedelete.cpp

--- rekketsuc++ programing/NewDelete.cpp
+++ rekketsuc++ programing/NewDelete.cpp
@@ -1,19 +1,16 @@
 #include<iostream>
 #include<string.h>
-#include<stdlib.h>
 
 using namespace std;
 
-
-char*MakeStrAdr(int len){
-    char*str=(chat*)malloc(sizeof(char)*len);
+char * MakeStrAdr(int len){
+    char * str = new char[len];
     return str;
 }
-
 int main(){
-    char*str = MakeStrAdr(20);
+    char * str = MakeStrAdr(20);
     strcpy(str,"I am so happy~");
     cout<<str<<endl;
-    free(str);
+    delete []str;
     return 0;
 }
