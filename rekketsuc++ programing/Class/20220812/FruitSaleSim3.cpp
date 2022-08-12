c87c4943b068ad60122a3ff59aab778123e2b5b2
livekill93 <livekill93@gmail.com>
2022-08-12 12:22:05 +0900

    Member initializer
    이니셜라이져를 이용하면 선언과 동시에 초기화가 이뤄지는 형태로 바이너리 코드가 생성된다.
    const맴버 변수도 이니셜라이저를 이용하면 초기화가 가능하다.

---
+++ rekketsuc++ programing/Class/20220812/FruitSaleSim3.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class FruitSeller
+{
+private:
+    /* data */
+    const int APPLE_PRICE;
+    int numofapples;
+    int mymoney;
+public:
+    FruitSeller(int price, int num, int money)
+        :APPLE_PRICE(price),
+        numofapples(num),
+        mymoney(money)
+    {
+
+    }
+    int SaleApples(int money){
+        int num = money/APPLE_PRICE;
+        numofapples -=num;
+        mymoney += money;
+        return num;
+    }
+    
+    void ShoiwsaleResult() const{
+        cout<<"남은 사과 : "<<numofapples<<endl;
+        cout<<"판매 수익 : "<<mymoney<<endl<<endl;
+    }
+};
+
+class FruitBuyer{
+    private:
+    int mymoney; //private
+    int numofapples; //private
+
+
+
+public:
+    FruitBuyer(int money)
+        :mymoney(money),
+        numofapples(0)
+        {
+    }
+    void BuyApples(FruitSeller &seller,int money){
+        numofapples+=seller.SaleApples(money);
+        mymoney-=money;
+    }
+    void ShoiwsaleResult() const{
+        cout<<"현재잔액 : "<<mymoney<<endl;
+        cout<<"사과 개수 : "<<numofapples<<endl<<endl;
+    }
+};
+
+int main(void) {
+	FruitSeller seller(1000, 20, 0);
+    FruitBuyer buyer(5000);
+    buyer.BuyApples(seller,2000);
+
+    cout<<"과일 판매자의 현황"<<endl;
+    seller.ShoiwsaleResult();
+    cout<<"과일 구매자 현황"<<endl;
+    buyer.ShoiwsaleResult();
+	return 0;
+}
