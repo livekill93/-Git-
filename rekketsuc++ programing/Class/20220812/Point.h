a86a28067901a7fd4fe382d5809af857c884dc11
livekill93 <livekill93@gmail.com>
2022-08-12 11:49:13 +0900

    Member Initializer
    맴버 이니셜라이즈를 이용한 초기화
    생성자를 추가해보자

---
+++ rekketsuc++ programing/Class/20220812/Point.h
@@ -0,0 +1,14 @@
+#ifndef __POINT_H_
+#define __POINT_H_
+
+class Point{
+    private:
+    int x,y;
+    public:
+    Point(const int &xpos,const int &ypo);
+    int GetX() const;
+    int GetY() const;
+    bool SetX(int xpos);
+    bool SetY(int ypos);
+};
+#endif
