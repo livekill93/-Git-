edc40d5826997bb1d2e2a7de46ba8d1d3a443b93
livekill93 <livekill93@gmail.com>
2022-08-13 23:50:01 +0900

    Create 3003.cpp

---
+++ 20220813/3003.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+//체스판?
+int p,l,q,b,k,n;
+p=0;
+l=0;
+q=0;
+b=0;
+k=0;
+n=0;
+
+cin>>k>>q>>l>>b>>n>>p>>endl;
+cout<<1-k<<' '<<1-q<<' '<<2-l<<' '<<2-b<<' '<<2-n<<' '<<8-p<<endl;
+
+return 0;
+}
