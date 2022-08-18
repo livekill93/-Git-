ceb6ca8f4ea123083efd773c31872449efd34494
livekill93 <livekill93@gmail.com>
2022-08-18 18:45:22 +0900

    Create code2-5.cs

---
+++ C#Programming/Part 01/chapter 02/code2-5.cs
@@ -0,0 +1,14 @@
+using System;//나머지 연산자
+namespace FristProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(10 % 55);
+            Console.WriteLine(7 % 3);
+            
+
+        }
+    }
+}
