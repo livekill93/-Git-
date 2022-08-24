daf4fc5fbfb0cbff3e254156760a1a348ac136e5
livekill93 <livekill93@gmail.com>
2022-08-24 20:24:19 +0900

    입력
    입력과 출력

---
+++ C#Programming/Part 01/chapter 02/code2-55.cs
@@ -0,0 +1,16 @@
+//입력
+//문자열 입력과 출력
+using System;
+namespace fristscript
+{
+    class  Program
+    {
+    static void Main(string[] args)
+    {   
+        string input = Console.ReadLine();
+        Console.WriteLine("input:" + input);
+
+    }
+    }
+    
+}
