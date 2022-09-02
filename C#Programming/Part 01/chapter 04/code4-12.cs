4d244a14a0e60055dfcdc8969949b049544c00c9
livekill93 <livekill93@gmail.com>
2022-09-02 22:39:36 +0900

    Create code4-12.cs

---
+++ C#Programming/Part 01/chapter 04/code4-12.cs
@@ -0,0 +1,14 @@
+using System;
+    class HelloWorld 
+    {
+    static void Main(string[] args) 
+        {
+        int output =0;
+    
+            for(int i= 0 ; i<= 100 ; i++)
+           {
+          output += i;
+          }
+        Console.WriteLine(output);
+        }
+    }
