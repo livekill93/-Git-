494f13ed6c314b44bc3606c4fe674b20c6a599b4
livekill93 <livekill93@gmail.com>
2022-09-07 00:01:24 +0900

    Create code4-19.cs

---
+++ C#Programming/Part 01/chapter 04/code4-19.cs
@@ -0,0 +1,22 @@
+//for 코드조각
+
+using System;
+
+namespace forloop
+{
+    class timrdate
+    {
+        static void Main  (string[] args)
+        {
+            
+         for( int i= 0; i< length>; i++)
+            {
+                
+            }
+            
+            
+            
+        }
+    }
+    
+}
