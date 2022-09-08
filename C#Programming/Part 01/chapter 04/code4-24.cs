0077ae11b969ec3f79e48991cc1b60b76e51fd1e
livekill93 <livekill93@gmail.com>
2022-09-08 20:48:01 +0900

    Create code4-24.cs

---
+++ C#Programming/Part 01/chapter 04/code4-24.cs
@@ -0,0 +1,19 @@
+//컨티뉴
+
+using System;
+namespace pc {
+    class propro
+    {
+        static void Main()
+        {
+            for (int i = 0 ; i < 19 ; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+} 
