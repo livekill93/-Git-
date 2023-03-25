afef8192eff1b26b1e8e64e906f4e65720e5ca34
livekill93 <livekill93@gmail.com>
2023-03-25 16:57:20 +0900

    배경 이미지 시차

--- Heropy-coffee/Js/main.js
+++ Heropy-coffee/Js/main.js
@@ -15,6 +15,7 @@ searchInputEl.addEventListener('blur', function () {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
   });
+
 //페이지 스크롤에 따른 요소 제어
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -34,6 +35,8 @@ window.addEventListener('scroll', function(){
     });
   }
 });
+
+
 //나타날 요소(.fade-in)들을 찾기
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 //요소를 하나씩 반복처리
@@ -43,6 +46,7 @@ fadeEls.forEach(function(fadeEls,index){
     opacity : 1
   });
 });
+
 /*swiper api*/
 new Swiper('.notice .swiper',{
   direction: 'vertical',//수직 슬라이드
@@ -54,7 +58,25 @@ new Swiper('.promotion .swiper',{
   loop:true,
   slidesPerView:3,
   spaceBetween:10,
-  centeredSlides: true
+  centeredSlides: true,
+  pagination: { // 페이지 번호 사용
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소
+    clickable: true // 사용자의 페이지 번호 제어 여부
+  },
+  navigation: { // 슬라이드 이전/다음 버튼 사용
+    prevEl: '.promotion .swiper-button-prev', // 이전 버튼 요소
+    nextEl: '.promotion .swiper-button-next' // 다음 버튼 요소
+  }
+});
+new Swiper('.awards .swiper', {
+  autoplay: true, // 자동 재생 여부
+  loop: true, // 반복 재생 여부
+  spaceBetween: 30, // 슬라이드 사이 여백
+  slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+  navigation: { // 슬라이드 이전/다음 버튼 사용
+    prevEl: '.awards .swiper-button-prev', // 이전 버튼 요소
+    nextEl: '.awards .swiper-button-next' // 다음 버튼 요소
+  }
 });
 
 // 슬라이드 영역 요소 검색!
@@ -70,35 +92,50 @@ promotionToggleBtn.addEventListener('click', function () {
     promotionEl.classList.add('hide');
   }
 });
-gasp.to(',floating1',1.5,{
+
+/* 부유요소  class에 show가 아떴던이유 이부분이 문제였음 */
+/*gsap가아니고 gsap였다...*/
+gsap.to('.floating1',1.5,{/*,랑 . 주의*/ 
   delay:1,
   y:15,
   repeat: -1,
   yoyo: true,
   ease: Power1.easeInOut
 });
-gasp.to(',floating2',2,{
-  delay:5,
-  y:15,
+gsap.to('.floating2', 2, {
+  delay: .5,
+  y: 15,
   repeat: -1,
   yoyo: true,
   ease: Power1.easeInOut
 });
-gasp.to(',floating3',2.5,{
-  delay:1.5,
-  y:20,
+gsap.to('.floating3', 2.5, {
+  delay: 1.5,
+  y: 20,
   repeat: -1,
   yoyo: true,
   ease: Power1.easeInOut
 });
 
+/*요소가 화면에 보여짐 여부에 따른 요소 관리 */
+/**
+ * 요소가 화면에 보여짐 여부에 따른 요소 관리
+ */
+// 관리할 요소들 검색!
 const spyEls = document.querySelectorAll('section.scroll-spy');
-spyEls.forEach(function(spyEls){
-  new Scrollmagic
-    .scene({
-      triggerElement:spyEls,
-      triggerHook: .8
+// 요소들 반복 처리!
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
     })
-    ,setClassToggle(spyEls,'show')
-    .addTo(new Scrollmagic.Controller());
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
 });
+
+/**
+ * 올해가 몇 년도인지 계산
+ */
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
